acfd2ef3e377adbb8db9f1f8e4ea35826ebd6d2b
steve0x <admin@metavault.org>
2023-10-30 13:34:46 +0100

    add tooltip banner

--- src/config/events.js
+++ src/config/events.js
@@ -1,4 +1,17 @@
 let eventsData = [
+  {
+    id: "kinetix-dapp-migration",
+    title: "Kinetix dApps Migration",
+    isActive: true,
+    validTill: "12/31/2023 12:00:00 PM",
+    bodyText: "We are deprecating the old dApps, please use our new dApp (DeFi Hub)",
+    buttons: [
+      {
+        text: "Kinetix DeFi-Hub",
+        link: "https://kinetix.finance",
+      }
+    ],
+  },
   // {
   //   id: "kinetix-trade-verified",
   //   title: "kinetix Official Domains",
